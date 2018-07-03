3afff5ac5be49315c4b069d4b9b1838c9d7f7a4a
YashaswiRahut <40323321+YashaswiRahut@users.noreply.github.com>
2018-07-03 17:56:03 +0530

    Add files via upload

---
+++ rain_drop_test1.ino
@@ -0,0 +1,11 @@
+void setup(){
+ Serial.begin(9600);
+ pinMode(A0, OUTPUT);
+}
+void loop(){
+ if(analogRead(A0)<300&&analogRead(A0)>0) Serial.println("heavy rain");
+ else if(analogRead(A0)<500&&analogRead(A0)>300) Serial.println("Moderate Rain");
+ else Serial.println("No Rain");
+
+
+delay(1000);}
