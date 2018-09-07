708e6008577ea468f14836a1bc655ea6d4670d3f
Yashaswi Rahut <yashaswirahut02@gmail.com>
2018-09-07 10:33:35 +0530

    Add files via upload

---
+++ hygrometer.ino
@@ -0,0 +1,20 @@
+int AnalogPin = 0; 
+int LEDpin = 11;      
+int Reading;      
+void setup(void) 
+{
+  Serial.begin(9600);   // We'll send debugging information via the Serial monitor
+  pinMode(LEDpin, OUTPUT);
+}
+ 
+void loop(void) {
+  Reading = analogRead(AnalogPin);
+  Serial.print("Analog reading = ");
+  Serial.println(Reading);
+if(Reading<200)
+Serial.print("needs water"), //connect your motor pins and turn on the motor
+delay(50000);
+else
+Serial.print("the plants is watered");
+delay(100);
+}
